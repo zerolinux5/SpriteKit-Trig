d52c7ea6262103d5759fc114e560d71f326da27b
Jesus Magana <maganazero5@gmail.com>
2014-07-02 14:38:50 -0700

    Initial Commit

---
+++ TrigBlasterSK/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrigBlasterSK
+//
+//  Created by Jesus Magana on 7/2/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
