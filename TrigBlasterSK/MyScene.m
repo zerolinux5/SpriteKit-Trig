d63108bf1a5b231fccf5ea4974f54f5b651e779a
Jesus Magana <maganazero5@gmail.com>
2014-07-02 14:59:56 -0700

    Ship now also rotates as it moves

--- TrigBlasterSK/MyScene.m
+++ TrigBlasterSK/MyScene.m
@@ -9,6 +9,9 @@
 @import CoreMotion;
 #import "MyScene.h"
 
+#define SK_DEGREES_TO_RADIANS(__ANGLE__) ((__ANGLE__) * 0.01745329252f) // PI / 180
+#define SK_RADIANS_TO_DEGREES(__ANGLE__) ((__ANGLE__) * 57.29577951f) // PI * 180
+
 const float MaxPlayerAccel = 400.0f;
 const float MaxPlayerSpeed = 200.0f;
 
@@ -118,6 +121,8 @@ const float MaxPlayerSpeed = 200.0f;
     newY = MIN(_winSize.height, MAX(newY, 0));
     
     _playerSprite.position = CGPointMake(newX, newY);
+    float angle = atan2f(_playerSpeedY, _playerSpeedX);
+    _playerSprite.zRotation = angle - SK_DEGREES_TO_RADIANS(90);
 }
 
 
